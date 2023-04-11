6c37b27a89e5b9e3f1cd6b2007f737bda11e5bc3
Mark Dietrich <MarkADietr@gmail.com>
2023-04-11 13:31:16 -0400

    created deck

--- Blackjack.py
+++ Blackjack.py
@@ -1,8 +1,28 @@
 import curses
 from curses import wrapper
-from curses.textpad import Tectangle
-import time
+import random
+
+suits = ['♥', '♦', '♣', '♠']
+values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q',' K' ]
+
+def CreateADeck():
+    deck = []
+    for s in suits:
+        for v in values:
+            deck.append((v+s))
+    return deck
+
+deck = CreateADeck()
+
 
 def main(stdscr):
-    pass
+    stdscr.clear()
+    stdscr.addstr( 5, 10, 'BLACKJACK')
+    stdscr.addstr( 7, 2, 'press any key to continue')
+    stdscr.refresh()
+    stdscr.getkey()
+    stdscr.addstr(f'deck:{deck}')
+    stdscr.refresh()
+    stdscr.getkey()
+
 wrapper(main)
