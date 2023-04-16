6d3017846fd3559bc29d585b015f3b4b17b203a2
Mark Dietrich <MarkADietr@gmail.com>
2023-04-15 20:39:38 -0400

    minor fixes in card and deck

--- Deck.py
+++ Deck.py
@@ -1,20 +1,25 @@
 from Card import Card
 import random
 
-
 class Deck:
-    SUITS = ['♥', '♦', '♣', '♠']
-    VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q','K' ]
 
-    def __init__(deck):
-        deck.cards = []
-        for s in SUITS:
-            for v in VALUES:
-                card = Card(suit,value)
-                deck.cards.append(card)
+    def __init__(self):
+        self.cards = []
+        self.build()
+        
+        
+    def build(self):
+        for s in ['♥', '♦', '♣', '♠']:
+            for v in ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q','K' ]:
+                self.cards.append(Card(s,v))
 
-    def DrawCard(deck):
-        i = random.randint(0, len(deck.cards))
-        drawnCard = deck.pop(i)
-        return [drawnCard]
+
+    def DrawCard(self):
+        i = random.randint(0, len(self.cards)-1)
+        drawnCard = self.cards.pop(i)
+        return drawnCard
     
+    def show(self):
+        for c in self.cards:
+            c.show()
+ 
--- Game.py
+++ Game.py
@@ -8,6 +8,7 @@ class Game:
         game.dealerHand = PokerHand()
         game.deck = Deck()
         game.maxVal = 21
+        game.playerStatus = 'P'
     
     def Deal(game):
         for x in range(2):
@@ -16,7 +17,7 @@ class Game:
     
     def PlayerTurn(game):
         playerInput = ''
-        while playerInput.lower() !== 's'
+        while playerInput.lower() != 's':
             stdscr.addstr( 7, 10, 'Press H to Hit')
             stdscr.addstr( 8, 9, 'Press S to Stand')
             playerInput = stdscr.getch()
@@ -27,20 +28,24 @@ class Game:
         playerCard = game.deck.DrawCard()
         game.playerHand.AddCard(playerCard)
         
-        playerCount = game.pokerHand.GetHandValue()
-        if game.CheckLoss(playerCount):
-            
-        
+        playerCount = game.playerHand.GetHandValue()
+        if game.ExceedMaxValue(playerCount):
+            game.playerStatus = 'L'   
 
     def DealerDrawCard(game):
         dealerCard = game.deck.DrawCard()
         game.dealerHand.AddCard(dealerCard)
 
-    def CheckLoss(game, count):
+        dealerCount = game.dealerHand.GetHandValue()
+        if game.ExceedMaxValue(dealerCount):
+            game.playerStatus = 'W'   
+
+
+    def ExceedMaxValue(game, count):
         if count > game.maxVal:
             return True
         else: 
             return False
 
 
-            
+Game.deck.show()
